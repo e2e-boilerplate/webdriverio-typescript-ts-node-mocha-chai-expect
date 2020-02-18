0250ee9ff4d8d43c1e452af04d8a16286e53a998
Girma Nigusse <xgirma@gmail.com>
2020-02-17 23:11:59 -0500

    cleanup

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,20 +1,48 @@
-exports.config = {
-    runner: 'local',
-    path: '/',
-    specs: [
-        './spec/*.spec.ts'
-    ],
-    capabilities: [{
-        browserName: 'chrome',
-    }],
-    logLevel: 'silent',
-    services: ['chromedriver'],
-    framework: 'mocha',
-    reporters: ['dot'],
-    mochaOpts: {
-        timeout: 60000
-    },
-    before: function (capabilities, specs) {
-        require('ts-node').register({ files: true });
-    },
+const headed = {
+  runner: "local",
+  path: "/",
+  specs: ["./spec/*.spec.ts"],
+  capabilities: [
+    {
+      browserName: "chrome"
+    }
+  ],
+  logLevel: "silent",
+  services: ["chromedriver"],
+  framework: "mocha",
+  reporters: ["dot"],
+  mochaOpts: {
+    timeout: 60000
+  },
+  before: function(capabilities, specs) {
+    require("ts-node").register({ files: true });
+  }
 };
+
+const headless = {
+  runner: "local",
+  path: "/",
+  specs: ["./spec/*.spec.ts"],
+  capabilities: [
+    {
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["--headless", "--disable-gpu"]
+      }
+    }
+  ],
+  logLevel: "silent",
+  services: ["chromedriver"],
+  framework: "mocha",
+  reporters: ["dot"],
+  mochaOpts: {
+    timeout: 60000
+  },
+  before: function(capabilities, specs) {
+    require("ts-node").register({ files: true });
+  }
+};
+
+const config = process.env.GITHUB_ACTIONS ? headless : headed;
+
+exports.config = config;
